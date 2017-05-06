76e1c799224f109868d55b312324b267ff56ea94
Xiaojie LI <ajie.lee@gmail.com>
2017-05-06 13:52:42 +0200

    Activate QuoteInput component

--- src/components/QuoteInput.vue
+++ src/components/QuoteInput.vue
@@ -4,9 +4,9 @@
       <form>
         <div class="form-group">
           <label for="quoteInput">Quote</label>
-          <textarea id="quoteInput" class="form-control" style="height: 100px; margin-bottom: 15px;"></textarea>
+          <textarea id="quoteInput" class="form-control" v-model="quote"></textarea>
           <div class="text-center">
-            <button class="btn btn-lg btn-primary" type="button">Add Quote</button>
+            <button class="btn btn-lg btn-primary" type="button" @click="addQuote">Add Quote</button>
           </div>
         </div>
       </form>
@@ -15,9 +15,42 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: {
+      quotes: {
+        type: Array,
+        required: true
+      },
+      max: {
+        type: Number,
+        required: true
+      }
+    },
+    data() {
+      return {
+        quote: ''
+      }
+    },
+    methods: {
+      addQuote() {
+        if (this.quote === '') {
+          return 0;
+        }
+
+        if (this.quotes.length >= this.max) {
+          alert('Too many quotes! Delete some before adding new ones!');
+          return 0;
+        }
+
+        this.quotes.push(this.quote);
+        this.quote = '';
+      }
+    }
+  }
 </script>
 
-<style>
-
+<style scoped>
+  textarea {
+    height: 100px; margin-bottom: 15px;
+  }
 </style>
